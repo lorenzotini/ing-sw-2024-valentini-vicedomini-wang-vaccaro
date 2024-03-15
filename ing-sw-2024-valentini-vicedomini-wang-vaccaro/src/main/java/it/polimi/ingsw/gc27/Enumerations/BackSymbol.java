cd9912c345508395fa0546932e6f80835fd7dacb
lorenzotini <lorenzo2.valentini@mail.polimi.it>
2024-03-15 09:51:58 +0100

    deleted CornerPosition.java and BackSymbol.java

--- ing-sw-2024-valentini-vicedomini-wang-vaccaro/src/main/java/it/polimi/ingsw/gc27/Enumerations/BackSymbol.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.gc27.Enumerations;
-
-public enum BackSymbol {
-    EMPTY,
-    PLANTKINGDOM,
-    ANIMALKINGDOM,
-    INSECTKINGDOM,
-    FUNGIKINGDOM
-}
--- ing-sw-2024-valentini-vicedomini-wang-vaccaro/src/main/java/it/polimi/ingsw/gc27/Enumerations/CornerPosition.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.gc27.Enumerations;
-
-public enum CornerPosition {
-    UPPERRIGHT,
-    UPPERLEFT,
-    LOWERRIGHT,
-    LOWERLEFT
-}

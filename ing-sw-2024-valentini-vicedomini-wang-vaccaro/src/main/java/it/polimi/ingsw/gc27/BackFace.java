af452e5370d98ccf418390fa755560fd8736aefa
Toni Wang <toni.wang@mail.polimi.it>
2024-03-16 18:06:01 +0100

    changes to the objective and other points

--- ing-sw-2024-valentini-vicedomini-wang-vaccaro/src/main/java/it/polimi/ingsw/gc27/BackFace.java
+++ ing-sw-2024-valentini-vicedomini-wang-vaccaro/src/main/java/it/polimi/ingsw/gc27/BackFace.java
@@ -1,10 +1,14 @@
 package it.polimi.ingsw.gc27;
-
+import java.util.*;
 public class BackFace extends  Face{
     private ArrayList<Kingdom> permanentResources;
 
     public BackFace(Kingdom colour, Corner cornerUR, Corner cornerUL, Corner cornerLR, Corner cornerLL, ArrayList<Kingdom> permanentResources) {
         super(colour, cornerUR, cornerUL, cornerLR, cornerLL);
         this.permanentResources = permanentResources;
+    }
+
+    public ArrayList<Kingdom> getPermanentResources() {
+        return permanentResources;
     }
 }
--- ing-sw-2024-valentini-vicedomini-wang-vaccaro/src/main/java/it/polimi/ingsw/gc27/Manuscript.java
+++ ing-sw-2024-valentini-vicedomini-wang-vaccaro/src/main/java/it/polimi/ingsw/gc27/Manuscript.java
@@ -1,5 +1,5 @@
 package it.polimi.ingsw.gc27;
-import java.util.ArrayList;
+import java.util.*;
 import it.polimi.ingsw.gc27.Enumerations.*;
 public class Manuscript {
     public final int FIELD_DIM = 81;
@@ -42,4 +42,127 @@ public class Manuscript {
     public void setyMin(int x) {
         this.yMin = x;
     }
+
+
+    public void addPointsObjective(ObjectiveCard objective, Board board, Player player) {
+        int points = 0;
+        int count = 0;
+
+        if(objective.getPattern().equals(ObjectiveRequirementType.THREEPLANTKINGDOM)) {
+            count = countCornerSymbol();
+        }
+        if(objective.getPattern().equals(ObjectiveRequirementType.THREEANIMALKINGDOM)) {
+
+        }
+        if(objective.getPattern().equals(ObjectiveRequirementType.THREEINSECTKINGDOM)) {
+
+        }
+        if(objective.getPattern().equals(ObjectiveRequirementType.THREEFUNGIKINGDOM)) {
+
+        }
+
+
+        if(objective.getPattern().equals(ObjectiveRequirementType.DOUBLEINKWELL) ||
+                objective.getPattern().equals(ObjectiveRequirementType.DOUBLEMANUSCRIPT) ||
+                objective.getPattern().equals(ObjectiveRequirementType.DOUBLEQUILL)
+        ) {
+
+        }
+
+
+        if(objective.getPattern().equals(ObjectiveRequirementType.EACHDIFFERENTTYPE)) {
+
+        }
+
+
+        if(objective.getPattern().equals(ObjectiveRequirementType.BLUELADDER) ||
+                objective.getPattern().equals(ObjectiveRequirementType.GREENLADDER) ||
+                objective.getPattern().equals(ObjectiveRequirementType.PURPLELADDER) ||
+                objective.getPattern().equals(ObjectiveRequirementType.REDLADDER)
+        ) {
+
+        }
+
+        if(objective.getPattern().equals(ObjectiveRequirementType.TWOPLUSONELOWERLEFT) ||
+                objective.getPattern().equals(ObjectiveRequirementType.TWOPLUSONELOWERRIGHT) ||
+                objective.getPattern().equals(ObjectiveRequirementType.TWOPLUSONEUPPERLEFT) ||
+                objective.getPattern().equals(ObjectiveRequirementType.TWOPLUSONEUPPERRIGHT)
+        ) {
+
+        }
+
+
+
+    }
+
+    public int countCornerSymbol(CornerSymbol x) {
+        int count = 0;
+
+        for(int i = (FIELD_DIM/2) - xMin ; i <= (FIELD_DIM/2) + xMax; i++) {
+            for(int j = (FIELD_DIM/2) - yMin ; j <= (FIELD_DIM/2) + yMax; j++) {
+                if(field[i][j] != null) {
+                    if(!field[i][j].getCornerLL().isHidden()){
+                        if(field[i][j].getCornerLL().getSymbol().equals(x)){
+                            count = count + 1;
+                        }
+                    }
+                    if(!field[i][j].getCornerUL().isHidden()){
+                        if(field[i][j].getCornerUL().getSymbol().equals(x)){
+                            count = count + 1;
+                        }
+                    }
+                    if(!field[i][j].getCornerUR().isHidden()){
+                        if(field[i][j].getCornerUR().getSymbol().equals(x)){
+                            count = count + 1;
+                        }
+                    }
+                    if(!field[i][j].getCornerLR().isHidden()){
+                        if(field[i][j].getCornerLR().getSymbol().equals(x)){
+                            count = count + 1;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public int countBackSymbol(Kingdom x) {
+        int count = 0;
+
+        for(int i = (FIELD_DIM/2) - xMin ; i <= (FIELD_DIM/2) + xMax; i++) {
+            for(int j = (FIELD_DIM/2) - yMin ; j <= (FIELD_DIM/2) + yMax; j++) {
+                if(field[i][j] != null && field[i][j] instanceof BackFace) {
+                    if(((BackFace) field[i][j]).getPermanentResources().contains(x)){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

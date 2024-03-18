5c35b207603b47e4c75ae182cc02c90f30c57bb8
lucreziavaccaro <lucrezia.vaccaro@mail.polimi.it>
2024-03-18 12:08:35 +0100

    method addPoints, setPoints player, countCoveredCorners implemented

--- ing-sw-2024-valentini-vicedomini-wang-vaccaro/src/main/java/it/polimi/ingsw/gc27/Manuscript.java
+++ ing-sw-2024-valentini-vicedomini-wang-vaccaro/src/main/java/it/polimi/ingsw/gc27/Manuscript.java
@@ -42,4 +42,92 @@ public class Manuscript {
     public void setyMin(int x) {
         this.yMin = x;
     }
+
+    /**
+     *
+     * @param player
+     
+     * @param board
+     * @param points
+     */
+    public void setPointsPlayer(Player player, Board board, int points){
+
+        PawnColour pawncolour = player.getPawnColour();
+        switch(pawncolour) {
+            case BLUE:
+                board.setPointsBluePlayer(board.getPointsBluePlayer() + points);
+                break;
+            case RED:
+                board.setPointsRedPlayer(board.getPointsRedPlayer() + points);
+                break;
+            case GREEN:
+                board.setPointsGreenPlayer(board.getPointsGreenPlayer() + points);
+                break;
+            case YELLOW:
+                board.setPointsYellowPlayer(board.getPointsYellowPlayer() + points);
+                break;
+        }
+    }
+
+    /**
+     *
+     * @param card
+     * @param x
+     * @param y
+     * @param board
+     * @return
+     */
+    public int countCoveredCorners(ResourceCard card, int x, int y, Board board){
+        int count=0;
+        Face ULFace = field[x-1][y+1];
+        if(ULFace.getCornerLR().isHidden()){
+            count++;
+        }
+        Face URFace = field[x+1][y+1];
+        if(URFace.getCornerLL().isHidden()){
+            count++;
+        }
+        Face LLFace = field[x-1][y-1];
+        if(LLFace.getCornerUR().isHidden()){
+            count++;
+        }
+        Face LRFace = field[x+1][y-1];
+        if(LRFace.getCornerUL().isHidden()){
+            count++;
+        }
+        return count;
+
+    }
+
+    /**
+     *
+     * @param player
+     * @param card
+     * @param face
+     * @param board
+     * @param numCoveredCorners
+     */
+    public void addPoints(Player player, ResourceCard card, Face face, Board board, int numCoveredCorners){
+        if(face instanceof FrontFace){
+            if (card instanceof ResourceCard){
+                setPointsPlayer(player, board, card.getCardPoints());
+            }
+            int points;
+            if (card instanceof GoldCard){
+                if(((GoldCard)card).getPointsMultiplier().equals(PointsMultiplier.CORNER)){
+                    points = card.getCardPoints() * numCoveredCorners;
+                }
+                else{
+                    points = card.getCardPoints() * countCornerSymbol(((GoldCard)card).getPointsMultiplier());
+                }
+            setPointsPlayer(player, board, points);
+        }
+
+
+
+    }
+
 }
+
+
+

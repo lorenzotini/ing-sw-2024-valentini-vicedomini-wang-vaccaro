06047ac503f13b0ee95a6c2b9cd479f7f638c520
lorenzotini <lorenzo2.valentini@mail.polimi.it>
2024-03-19 16:24:43 +0100

    add extentions to ObjectiveCard to implement strategy pattern (calculateObjectivePoints)

---
+++ ing-sw-2024-valentini-vicedomini-wang-vaccaro/src/main/java/it/polimi/ingsw/gc27/Card/ThreeKingdomPattern.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.gc27.Card;
+
+import it.polimi.ingsw.gc27.Enumerations.Kingdom;
+
+public class ThreeKingdomPattern extends ObjectiveCard{
+    private Kingdom kingdom;
+
+    @Override
+    public int calculateObjectivePoints() {
+
+    }
+}
---
+++ ing-sw-2024-valentini-vicedomini-wang-vaccaro/src/main/java/it/polimi/ingsw/gc27/Card/TwoPlusOnePattern.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.gc27.Card;
+
+import it.polimi.ingsw.gc27.Enumerations.Kingdom;
+
+public class TwoPlusOnePattern extends ObjectiveCard{
+    private Kingdom firstColour;
+    private Kingdom secondColour;
+    private int i;
+    private int j;
+    @Override
+    public int calculateObjectivePoints() {
+
+    }
+}

ad33c637c97684041fecf72fe4ca9cabf6688999
Toni Wang <toni.wang@mail.polimi.it>
2024-03-29 17:49:40 +0100

    new branch with views

---
+++ src/main/java/it/polimi/ingsw/gc27/View/View.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.gc27.View;
+
+public interface View {
+
+}
+
+
+/*
+* lanciare la UI (grafica o testuale)
+* connettersi
+* scegliere l'username
+* mostrare la board
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+* */
---
+++ src/main/java/it/polimi/ingsw/gc27/View/ViewCli.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.gc27.View;
+
+import java.util.ArrayList;
+
+public class ViewCli implements View {
+    private ArrayList<String> commandList;
+
+    public void showManuscript() {
+
+    }
+    public void showCommandList() {
+
+    }
+
+}

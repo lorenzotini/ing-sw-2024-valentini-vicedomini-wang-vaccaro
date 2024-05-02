fc15f609b0e73d0876893adef47ef84d67784aa6
Toni Wang <toni.wang@mail.polimi.it>
2024-05-02 15:58:06 +0200

    added Listeners placeholders

---
+++ src/main/java/it/polimi/ingsw/gc27/Listeners/EmailMsgListeners.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.gc27.Listeners;
+
+public class EmailMsgListeners implements EventListener{
+    private final String email;
+
+    public EmailMsgListeners(String email) {
+        this.email = email;
+    }
+    @Override
+    public void update() {
+        //...
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/gc27/Listeners/EventListener.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.gc27.Listeners;
+
+public interface EventListener {
+    void update();
+}

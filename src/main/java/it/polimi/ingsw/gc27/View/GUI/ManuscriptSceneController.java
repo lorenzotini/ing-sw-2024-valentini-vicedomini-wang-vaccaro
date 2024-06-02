238ea808c4cb0777a2f0d2ea08e019e71e4c044b
lorenzotini <lorenzo2.valentini@mail.polimi.it>
2024-06-03 00:34:05 +0200

    update ManuscriptScene

--- src/main/java/it/polimi/ingsw/gc27/View/GUI/ManuscriptSceneController.java
+++ src/main/java/it/polimi/ingsw/gc27/View/GUI/ManuscriptSceneController.java
@@ -6,17 +6,22 @@ import it.polimi.ingsw.gc27.Model.Card.GoldCard;
 import it.polimi.ingsw.gc27.Model.Card.ObjectiveCard.ObjectiveCard;
 import it.polimi.ingsw.gc27.Model.Card.ResourceCard;
 import it.polimi.ingsw.gc27.Model.Card.StarterCard;
+import it.polimi.ingsw.gc27.Model.Game.Manuscript;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.transform.Scale;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -27,12 +32,6 @@ public class ManuscriptSceneController implements GenericController {
     @FXML
     private ScrollPane scrollPane;
     @FXML
-    private ImageView handCard1;
-    @FXML
-    private ImageView handCard2;
-    @FXML
-    private ImageView handCard3;
-    @FXML
     private HBox handCards;
     @FXML
     private VBox counters;
@@ -41,22 +40,12 @@ public class ManuscriptSceneController implements GenericController {
     @FXML
     private HBox marketGolds;
     @FXML
-    private ImageView resourceDeck;
-    @FXML
-    private ImageView resource1;
-    @FXML
-    private ImageView resource2;
-    @FXML
-    private ImageView goldDeck;
-    @FXML
-    private ImageView gold1;
-    @FXML
-    private ImageView gold2;
-
+    private VBox commonObjectives;
 
 
     public void initialize() {
 
+        // shuffle decks
         ArrayList<StarterCard> deckStarter = JsonParser.getStarterDeck(JsonParser.cardsJsonObj);
         ArrayList<ResourceCard> deckResource = JsonParser.getResourceDeck(JsonParser.cardsJsonObj);
         ArrayList<GoldCard> deckGold = JsonParser.getGoldDeck(JsonParser.cardsJsonObj);
@@ -68,40 +57,51 @@ public class ManuscriptSceneController implements GenericController {
 
         GridPane grid = new GridPane();
         grid.setGridLinesVisible(true);
-
         grid.setHgap(-35);
         grid.setVgap(-40);
 
-//        grid.setPrefHeight(4000);
-//        grid.setPrefWidth(8000);
-//        grid.setMaxHeight(4000);
-//        grid.setMaxWidth(8000);
-//
-//        RowConstraints rowC = new RowConstraints();
-//        ColumnConstraints columnC = new ColumnConstraints();
-//
-//        rowC.setValignment(VPos.CENTER);
-//        columnC.setHalignment(HPos.CENTER);
-//
-//        grid.getRowConstraints().add(rowC);
-//        grid.getColumnConstraints().add(columnC);
-
         scrollPane.setContent(grid);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
 
-        // TODO sostituire gli estremi dei for con Manuscript.FIELD_DIM
+        // make the grid scalable
+        Scale scaleTransform = new Scale(1, 1, 0, 0);
+        grid.getTransforms().add(scaleTransform);
+        scrollPane.addEventFilter(ScrollEvent.SCROLL, event -> {
+            if (event.isControlDown()) { // Use Ctrl+Scroll for zooming
+                double deltaY = event.getDeltaY();
+                double zoomFactor = 1.05;
+                if (deltaY < 0) {
+                    zoomFactor = 0.95;
+                }
+                scaleTransform.setX(scaleTransform.getX() * zoomFactor);
+                scaleTransform.setY(scaleTransform.getY() * zoomFactor);
+                event.consume();
+            }
+        });
+
         // populate grid with imageViews
         boolean flag = true;
-        for (int i = 0; i < 11; i++) {       // requires odd terminal value to work, especially for the j loop
-            for (int j = 0; j < 11; j++) {
+        for (int i = 0; i < Manuscript.FIELD_DIM; i++) {       // requires odd terminal value to work, especially for the j loop
+            for (int j = 0; j < Manuscript.FIELD_DIM; j++) {
                 if (flag) {
 
                     ImageView imageView = new ImageView();
 
-                    // default image in order to make the imageView able to receive drag events
-                    imageView.setImage(new Image("/images/cards/card1_back.png"));
+                    // set image for the center starter card
+                    if (i == Manuscript.FIELD_DIM / 2 && j == Manuscript.FIELD_DIM / 2) {
+                        imageView.setImage(new Image(deckStarter.get(0).getFront().getImagePath()));
+                    }
 
-                    // handle drag events
-                    handleDropEvent(imageView);
+                    // playable positions
+                    // TODO implement isValidPlacement
+                    if ((i == Manuscript.FIELD_DIM / 2 - 1 || i == Manuscript.FIELD_DIM / 2 + 1) && (j == Manuscript.FIELD_DIM / 2 - 1 || j == Manuscript.FIELD_DIM / 2 + 1)) {
+                        imageView.setImage(new Image(getClass().getResource("/images/utility/validPlacement.png").toExternalForm()));
+                        // handle drag events
+                        handleDropEvent(imageView);
+                    }
 
                     imageView.setFitHeight(100);
                     imageView.setFitWidth(150);
@@ -113,17 +113,68 @@ public class ManuscriptSceneController implements GenericController {
             }
         }
 
-
         // populate hand with cards
-        Card c1 = deckGold.get(0);
-        Card c2 = deckGold.get(1);
-        Card c3 = deckGold.get(3);
-        handCard1.setImage(new Image(c1.getFront().getImagePath()));
-        handCard2.setImage(new Image(c2.getFront().getImagePath()));
-        handCard3.setImage(new Image(c3.getFront().getImagePath()));
-        handleDragDetected(handCard1);
-        handleDragDetected(handCard2);
-        handleDragDetected(handCard3);
+        for (int i = 0; i < 3; i++) {
+            ImageView handCard = new ImageView(new Image(deckGold.get(i).getFront().getImagePath()));
+            handCard.setFitHeight(100);
+            handCard.setFitWidth(150);
+            handleDragDetected(handCard);
+            zoomCardOnHover(handCard);
+            handCards.getChildren().add(handCard);
+        }
+
+        // market
+        // TODO controllare che vengano caricate in ordine e non al contrario
+        // TODO sembra che le carte non vengano tolte dal mazzo, capire perché
+        StackPane resourceDeckStackPane = new StackPane();
+        resourceDeckStackPane.setAlignment(Pos.TOP_CENTER);
+        for(Card card : deckResource) {
+            ImageView resourceDeckCard = new ImageView(new Image(card.getBack().getImagePath()));
+            resourceDeckCard.setFitHeight(50);
+            resourceDeckCard.setFitWidth(75);
+            handleDragDetected(resourceDeckCard);
+            zoomCardOnHover(resourceDeckCard);
+            resourceDeckStackPane.getChildren().add(resourceDeckCard);
+        }
+        marketResources.getChildren().add(resourceDeckStackPane);
+
+        StackPane goldDeckStackPane = new StackPane();
+        goldDeckStackPane.setAlignment(Pos.TOP_CENTER);
+        for(Card card : deckGold) {
+            ImageView goldDeckCard = new ImageView(new Image(card.getBack().getImagePath()));
+            goldDeckCard.setFitHeight(50);
+            goldDeckCard.setFitWidth(75);
+            handleDragDetected(goldDeckCard);
+            zoomCardOnHover(goldDeckCard);
+            goldDeckStackPane.getChildren().add(goldDeckCard);
+        }
+        marketGolds.getChildren().add(goldDeckStackPane);
+
+        for (int i = 0; i < 2; i++) {
+            // resources
+            ImageView marketResource = new ImageView(new Image(deckResource.get(i).getFront().getImagePath()));
+            marketResource.setFitHeight(50);
+            marketResource.setFitWidth(75);
+            handleDragDetected(marketResource);
+            zoomCardOnHover(marketResource);
+            marketResources.getChildren().add(marketResource);
+            // golds
+            ImageView marketGold = new ImageView(new Image(deckGold.get(i).getFront().getImagePath()));
+            marketGold.setFitHeight(50);
+            marketGold.setFitWidth(75);
+            handleDragDetected(marketGold);
+            zoomCardOnHover(marketGold);
+            marketGolds.getChildren().add(marketGold);
+        }
+
+        // common objectives
+        for (int i = 0; i < 2; i++) {
+            ImageView commonObjective = new ImageView(new Image(deckobjective.get(i).getFront().getImagePath()));
+            commonObjective.setFitHeight(100);
+            commonObjective.setFitWidth(150);
+            zoomCardOnHover(commonObjective);
+            commonObjectives.getChildren().add(commonObjective);
+        }
 
         // counters
         for (int i = 0; i < 7; i++) {
@@ -131,29 +182,6 @@ public class ManuscriptSceneController implements GenericController {
             counter.setEditable(false);
             counters.getChildren().add(counter);
         }
-
-        // market
-        Card goldDeckCard = deckGold.get(4);
-        Card gold1card = deckGold.get(5);
-        Card gold2card = deckGold.get(6);
-        goldDeck.setImage(new Image(goldDeckCard.getFront().getImagePath()));
-        gold1.setImage(new Image(gold1card.getFront().getImagePath()));
-        gold2.setImage(new Image(gold2card.getFront().getImagePath()));
-        handleDragDetected(goldDeck);
-        handleDragDetected(gold1);
-        handleDragDetected(gold2);
-
-        Card resourceDeckCard = deckResource.get(4);
-        Card resource1card = deckResource.get(5);
-        Card resource2card = deckResource.get(6);
-        resourceDeck.setImage(new Image(resourceDeckCard.getFront().getImagePath()));
-        resource1.setImage(new Image(resource1card.getFront().getImagePath()));
-        resource2.setImage(new Image(resource2card.getFront().getImagePath()));
-        handleDragDetected(resourceDeck);
-        handleDragDetected(resource1);
-        handleDragDetected(resource2);
-
-        //marketGolds.alignmentProperty().setValue(javafx.geometry.Pos.CENTER);
 
     }
 
@@ -163,7 +191,7 @@ public class ManuscriptSceneController implements GenericController {
         imgView.setOnDragOver(event -> {
             Dragboard db = event.getDragboard();
             if (db.hasImage() || db.hasFiles()) {
-                event.acceptTransferModes(TransferMode.ANY);
+                event.acceptTransferModes(TransferMode.MOVE);
             }
             event.consume();
         });
@@ -173,6 +201,7 @@ public class ManuscriptSceneController implements GenericController {
             Dragboard db = event.getDragboard();
             if (db.hasImage()) {
                 imgView.setImage(db.getImage());
+                event.setDropCompleted(true);
             }
             event.consume();
         });
@@ -183,11 +212,38 @@ public class ManuscriptSceneController implements GenericController {
 
         // DRAG DETECTED
         imgView.setOnDragDetected(event -> {
-            Dragboard db = imgView.startDragAndDrop(TransferMode.ANY);
+            Dragboard db = imgView.startDragAndDrop(TransferMode.MOVE);
             ClipboardContent cb = new ClipboardContent();
             cb.putImage(imgView.getImage());
             db.setContent(cb);
             event.consume();
+        });
+
+        // DRAG DONE
+        imgView.setOnDragDone(event -> {
+            if (event.getTransferMode() == TransferMode.MOVE) {
+                imgView.setImage(null);
+            }
+            event.consume();
+        });
+
+    }
+
+    void zoomCardOnHover(ImageView imgView){
+
+        double originalHeight = imgView.getFitHeight();
+        double originalWidth = imgView.getFitWidth();
+
+        double factor = 1.5 + (100-originalHeight)/originalHeight;
+
+        imgView.setOnMouseEntered(event -> {
+            imgView.setFitHeight(originalHeight*factor);
+            imgView.setFitWidth(originalWidth*factor);
+        });
+
+        imgView.setOnMouseExited(event -> {
+            imgView.setFitHeight(originalHeight);
+            imgView.setFitWidth(originalWidth);
         });
 
     }

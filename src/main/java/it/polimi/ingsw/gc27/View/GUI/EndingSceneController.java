a7221a12ef844b6b483da34d1ba0d7c6d9eedb5c
Toni Wang <toni.wang@mail.polimi.it>
2024-06-06 15:18:11 +0200

    added EndingScene, validPlacement2 utility image and other decorations

---
+++ src/main/java/it/polimi/ingsw/gc27/View/GUI/EndingSceneController.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.gc27.View.GUI;
+
+public class EndingSceneController implements GenericController{
+
+
+
+    
+
+    @Override
+    public void receiveOk(String ackType) {
+
+    }
+
+    @Override
+    public void receiveKo(String ackType) {
+
+    }
+}
